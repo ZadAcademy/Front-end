a39be89d46a0da5a2e7f849729f62a2e5789c87b
Mahmoud Nouh <117545515+ma7mod7@users.noreply.github.com>
2026-07-13 02:45:43 +0300

    fix(auth):edit types import path

--- src/features/auth/forget-password/hooks/use-reset-password-mutation.ts
+++ src/features/auth/forget-password/hooks/use-reset-password-mutation.ts
@@ -2,6 +2,7 @@ import { useMutation } from '@tanstack/react-query';
 import { resetPassword,  } from '../api/reset-password';
 import { ResetPasswordPayload } from '../lib/types/reset-password';
 
+
 export const useResetPasswordMutation = () => {
   return useMutation({
     mutationFn: (data: ResetPasswordPayload) => resetPassword(data),
