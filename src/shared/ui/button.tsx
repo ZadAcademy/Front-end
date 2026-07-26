8e5213fbec81ba1abcb4e6a83972170b5e38f38a
Mahmoud Nouh <117545515+ma7mod7@users.noreply.github.com>
2026-07-26 16:26:27 +0300

    fix(course) fix the color issue

--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -10,9 +10,9 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        primary: "bg-primary text-primary-foreground hover:bg-primary/80",
+        primary: "bg-primary text-white hover:bg-primary/80",
         outline:
-          "border-border  hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
+          "border-border text-foreground hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-[color-mix(in_oklch,var(--secondary),var(--foreground)_5%)] aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
         ghost:
